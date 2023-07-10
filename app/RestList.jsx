70eff2790da646712ae7a4cde4003cdfc7723ff4
Jeniffer Giraldo <tris@Jeniffers-MacBook-Air-2.local>
2023-07-10 11:54:12 -0400

    scrolling restaurant list

---
+++ app/RestList.jsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react"
+import { ScrollView, StyleSheet, Text } from "react-native"
+import RestCard from "./RestCard"
+
+export default function RestList() {
+    const [restaurants, setRestaurants] = useState()
+
+    useEffect(() => {
+        fetch(`https://api.bocacode.com/api/restaurants`)
+        .then(res => res.json())
+        .then(setRestaurants)
+        .catch(alert)
+    }, [])
+
+    return(
+        <>
+            <ScrollView style={styles.scrollingList}>
+                {!restaurants
+                 ? <Text styles={styles.loadingText}>Loading...</Text>
+                 : restaurants.map(restaurant => (
+                    <RestCard key={restaurant._id} restaurant={restaurant} />
+                 ))
+                }
+            </ScrollView>
+        </>
+    )
+}
+
+const styles = StyleSheet.create({
+    loadingText: {
+        padding: 8,
+        fontSize: 24,
+        color: '#eee',
+    },
+    scrollingList: {
+        // borderColor: 'pink',
+        // borderWidth: 2,
+        // borderStyle: 'solid',
+        width: '90%',
+        marginTop: 16,
+    },
+})
